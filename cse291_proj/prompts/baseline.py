224f7081e89d5a42625ab7e7ae2dd9a84ba159e2
Bhrugu Bharathi <bhrugu.bharathi@gmail.com>
2024-11-22 03:14:12 +0000

    Student File generation code for baselines, and later for struct chem. Need to fix a final formatting error with the question ID decoding.

---
+++ cse291_proj/prompts/baseline.py
@@ -0,0 +1,7 @@
+# // prompt_config = {
+# //     "prompt_template": "Please provide a clear and step-by-step solution for a scientific problem in the categories of Chemistry, Physics, or Mathematics. The problem will specify the unit of measurement, which should not be included in the answer. Express the final answer as a decimal number with three digits after the decimal point. Conclude the answer by stating "The answer is therefore \boxed{[ANSWER]}.""
+# // }
+
+baseline_cot_prompt_config = {
+    "prompt_template": "Please provide a clear and step-by-step solution for a scientific problem in the categories of Chemistry, Physics, or Mathematics. The problem will specify the unit of measurement, which should not be included in the answer. Express the final answer as a decimal number with three digits after the decimal point. Conclude the answer by stating \"The answer is therefore \\boxed{[ANSWER]}.\""
+}
